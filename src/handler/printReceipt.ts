c21a57b2724f696237c8e7b523537f0265966aa3
Riccardo Vecchi <rvecchi@byte-code.com>
2020-08-21 16:05:48 +0200

    Printing basket

--- src/handler/printReceipt.ts
+++ src/handler/printReceipt.ts
@@ -11,4 +11,18 @@ export const printReceipt = async (input: Input[]): Promise<void> => {
   }
 };
 
-export default printReceipt(input);
+console.log(`----------------------------------------------`);
+console.log('          HELLLOOOO FROM Riccardo             ');
+console.log('                                              ');
+console.log('          Here You can find the               ');
+console.log('          receipt of our basket.              ');
+console.log('          If you want to modify basket        ');
+console.log('          please go under src/data/input.json ');
+console.log('          and then modify that                ');
+console.log('          !!!ENJOY!!!                         ');
+console.log('                                              ');
+printReceipt(input).then(() => {
+console.log('Riccardo © All rights reserved :P');
+console.log(`----------------------------------------------`);
+});
+
--- src/service/print/receipt.ts
+++ src/service/print/receipt.ts
@@ -5,7 +5,7 @@ import { checkout } from '../cart/checkout';
 
 
 function writeItems(cart: Cart) {
-  console.log(`---------------- RECEIPT -------------------`);
+  console.log(`------------------ RECEIPT -------------------`);
   if(cart.items) {
     cart.items.forEach((item: Item) => {
       console.log(`${ item.quantity } ${ item.name }: ${ item.salePrice }`);
@@ -16,10 +16,10 @@ function writeItems(cart: Cart) {
 }
 
 function writeTotals(cart: Cart) {
-  console.log(`--------------------------------------------`);
+  console.log(`----------------------------------------------`);
   console.log(`Sales Taxes: ${cart.salesTaxes}`);
-  console.log(`Total${cart.total}`);
-  console.log(`--------------- END RECEIPT ----------------`);
+  console.log(`Total: ${cart.total}`);
+  console.log(`----------------- END RECEIPT ----------------`);
 }
 
 export default async (inputs: Input[]) => {
